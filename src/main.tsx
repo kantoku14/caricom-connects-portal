e2d0cf499bca519b88a7ccdc9a174c42cb7e323d
Jelton Mentore <fourteenyama@gmail.com>
2024-09-20 08:28:54 -0400

    Set up Appwrite, Chakra UI, CI/CD, and other frameworks

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// main.tsx or index.tsx
+import { ChakraProvider } from '@chakra-ui/react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import * as Sentry from '@sentry/react'
+
+Sentry.init({
+  dsn: 'https://02f55fc52e1bd034e48363376e41b9c5@o4507982706769920.ingest.us.sentry.io/4507985165352960',
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+})
+
+ReactDOM.render(
+  <ChakraProvider>
     <App />
-  </StrictMode>,
+  </ChakraProvider>,
+  document.getElementById('root')
 )
