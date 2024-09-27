5fc638db8e4039407de76e5cf078c33bbd162a33
Jelton Mentore <fourteenyama@gmail.com>
2024-09-27 01:05:05 -0400

    refactor: remove unused render import

--- src/__tests__/dom.test.ts
+++ src/__tests__/dom.test.ts
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 
 test('renders content', () => {
   document.body.innerHTML = '<div>hello world</div>';
